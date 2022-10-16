4c503a87da3df6855f69b6b9b5e3097373e83f4d
mitchel432 <mitchelh432@gmail.com>
2022-10-16 09:29:09 -0700

    Updated Corporation Page

--- themes/sara_theme/src/js/corporation.js
+++ themes/sara_theme/src/js/corporation.js
@@ -6,12 +6,14 @@
 (function ($) {
     Drupal.behaviors.corporation = {
       attach(context) {
+        let parent = $('.view-display-id-attachment_1');
         $('.view-corporation .corp__button', context).once('corporation').each((index, value) => {
             let corpId = value.dataset.corpId;
             $(value).on("click", function(event) {
                 event.preventDefault();
                 let className = ".corp__secondary-image__" + corpId;
                 $(className).slideToggle();
+                parent.addClass('active');
             });
         });
         $('.view-corporation .close-button', context).once('corporation').each((index, value) => {
@@ -20,6 +22,7 @@
                 event.preventDefault();
                 let className = ".corp__secondary-image__" + closeId;
                 $(className).slideToggle();
+                parent.removeClass('active');
             });
         });
     },
